bcc67cd18ad60c27da7219c62c706580c5149f8c
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-04-02 22:21:39 -0300

    implemented read terminal input

--- pocs/todo-kotlin/src/Main.kt
+++ pocs/todo-kotlin/src/Main.kt
@@ -1,10 +1,16 @@
-class Main {
-    fun run() {
-        println("Kotlin funcionando junto com Java!")
-    }
-}
-
 fun main() {
-    val app = Main()
-    app.run()
+    val manager = TaskManager()
+
+    while (true) {
+        println("\n--- TO-DO LIST ---")
+        println("1. Listar todas")
+        println("0. Sair")
+        print("Escolha: ")
+
+        when (readLine()?.trim()) {
+            "1" -> manager.listAll()
+            "0" -> { println("Até mais!"); break }
+            else -> println("Opção inválida.")
+        }
+    }
 }
