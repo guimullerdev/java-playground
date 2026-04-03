7f46feed99b242a93ac62fce852b5022b03b7e0a
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-04-02 22:18:12 -0300

    first commit todo kotlin

--- pocs/todo-kotlin/src/Main.kt
+++ pocs/todo-kotlin/src/Main.kt
@@ -1,16 +1,10 @@
-fun main() {
-    val manager = TaskManager()
-
-    while (true) {
-        println("\n--- TO-DO LIST ---")
-        println("1. Listar todas")
-        println("0. Sair")
-        print("Escolha: ")
-
-        when (readLine()?.trim()) {
-            "1" -> manager.listAll()
-            "0" -> { println("Até mais!"); break }
-            else -> println("Opção inválida.")
-        }
+class Main {
+    fun run() {
+        println("Kotlin funcionando junto com Java!")
     }
+}
+
+fun main() {
+    val app = Main()
+    app.run()
 }
