dca0bf84f502416d2f83e7eb1b58f4c23ff9fd79
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-04-02 22:27:31 -0300

    implemented option to delete task

--- pocs/todo-kotlin/src/Main.kt
+++ pocs/todo-kotlin/src/Main.kt
@@ -4,11 +4,19 @@ fun main() {
     while (true) {
         println("\n--- TO-DO LIST ---")
         println("1. Listar todas")
+        println("2. Remover tarefa")
         println("0. Sair")
         print("Escolha: ")
 
         when (readLine()?.trim()) {
             "1" -> manager.listAll()
+            "2" -> {
+                println("Digite o id da tarefa")
+                var id = readLine()?.toIntOrNull();
+                if(id != null) {
+                    manager.delete(id);
+                }
+            }
             "0" -> { println("Até mais!"); break }
             else -> println("Opção inválida.")
         }
