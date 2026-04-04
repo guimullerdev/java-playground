cefca2d05b67f4cf835d5b67258ecd61e7b94780
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-04-03 23:25:40 -0300

    implemented reader to sum

--- pocs/calculator/src/Main.kt
+++ pocs/calculator/src/Main.kt
@@ -1,3 +1,29 @@
 fun Main() {
+    val calculator = Calculator();
 
+    while(true) {
+        val input = readLine()?.trim()
+        println("\n--- TO-DO LIST ---")
+        println("1. Somar")
+        println("2. Subatrair")
+        println("0. Sair")
+        print("Escolha: ")
+
+        when (readLine()?.trim()) {
+            "1" -> {
+                print("Numero 1: ")
+                val number1 = input?.toInt()
+                print("Numero 2: ")
+                val number2 = input?.toInt()
+                if (number1 != null && number2 != null) {
+                    calculator.sum(number1, number2)
+                }
+            }
+            "2" -> {
+
+            }
+            "0" -> { println("Até mais!"); break }
+            else -> println("Opção inválida.")
+        }
+    }
 }
