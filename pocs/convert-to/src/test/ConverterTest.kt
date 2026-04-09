d72482b18fce5c9840f8836e3b05adaacb51ad2c
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-04-08 22:52:04 -0300

    implemented test to service converter

---
+++ pocs/convert-to/src/test/ConverterTest.kt
@@ -0,0 +1,15 @@
+package test
+
+import org.jetbrains.annotations.TestOnly
+
+class ConverterTest {
+
+    private val delta = 0.001
+
+    @TestOnly
+    fun `meter to km`() {
+//        val result = Converter.distance(1000.0, DistanceUnit.METER, DistanceUnit.KM)
+//        assertEquals(1.0, result, delta)
+    }
+
+}
